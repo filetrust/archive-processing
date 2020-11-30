dcb57493cdc9758c76f13d10cfa55a8c91ef9870
Matt <matt.dignum@googlemail.com>
2020-11-30 15:24:09 +0000

    Send error report if archive is password protected

---
+++ Source/Service/ErrorReport/HtmlPasswordProtectedErrorReportGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Service.ErrorReport
+{
+    public class HtmlPasswordProtectedErrorReportGenerator : IPasswordProtectedReportGenerator
+    {
+        public string CreateReport(string fileId)
+        {
+            using (var reader = File.OpenText("Templates/PasswordProtectedErrorReportTemplate.html"))
+            {
+                var report = reader.ReadToEnd();
+
+                var builder = new StringBuilder(report);
+
+                builder.Replace("{{FILE_ID}}", $"{fileId}");
+
+                return builder.ToString();
+            }
+        }
+    }
+}
