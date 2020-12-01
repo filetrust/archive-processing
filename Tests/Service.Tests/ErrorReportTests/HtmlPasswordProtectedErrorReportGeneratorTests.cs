101238b8ff4e42c80d7003834f375641a264b802
Matt <matt.dignum@googlemail.com>
2020-12-01 16:10:39 +0000

    Tests

--- Tests/Service.Tests/ErrorReportTests/HtmlPasswordProtectedErrorReportGeneratorTests.cs
+++ Tests/Service.Tests/ErrorReportTests/HtmlPasswordProtectedErrorReportGeneratorTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using Service.ErrorReport;
 using System;
+using Moq;
+using Service.Configuration;
 
 namespace Service.Tests
 {
@@ -12,15 +14,57 @@ namespace Service.Tests
             public void Id_Is_Added_To_The_Report_Template()
             {
                 // Arrange
+                var config = new Mock<IArchiveProcessorConfig>();
+
                 var expectedId = Guid.NewGuid().ToString();
 
-                var reportGenerator = new HtmlPasswordProtectedErrorReportGenerator();
+                var reportGenerator = new HtmlPasswordProtectedErrorReportGenerator(config.Object);
 
                 // Act
                 var result = reportGenerator.CreateReport(expectedId);
 
                 // Assert
                 Assert.That(result, Contains.Substring(expectedId));
+            }
+
+            [Test]
+            public void Custom_Error_Is_Added_To_The_Report_Template()
+            {
+                // Arrange
+                var config = new Mock<IArchiveProcessorConfig>();
+
+                var expectedId = Guid.NewGuid().ToString();
+                var expectedErrorMessage = "Error Should Be Added To Report";
+
+                config.SetupGet(s => s.ArchivePasswordProtectedReportMessage).Returns(expectedErrorMessage);
+
+                var reportGenerator = new HtmlPasswordProtectedErrorReportGenerator(config.Object);
+
+                // Act
+                var result = reportGenerator.CreateReport(expectedId);
+
+                // Assert
+                Assert.That(result, Contains.Substring(expectedErrorMessage));
+            }
+
+            [Test]
+            public void Default_Error_Is_Added_To_The_Report_Template_If_Config_Is_Null()
+            {
+                // Arrange
+                var config = new Mock<IArchiveProcessorConfig>();
+
+                var expectedId = Guid.NewGuid().ToString();
+                var defaultError = "The archive was password protected and unable to be analysed";
+
+                config.SetupGet(s => s.ArchivePasswordProtectedReportMessage).Returns((string)null);
+
+                var reportGenerator = new HtmlPasswordProtectedErrorReportGenerator(config.Object);
+
+                // Act
+                var result = reportGenerator.CreateReport(expectedId);
+
+                // Assert
+                Assert.That(result, Contains.Substring(defaultError));
             }
         }
     }
